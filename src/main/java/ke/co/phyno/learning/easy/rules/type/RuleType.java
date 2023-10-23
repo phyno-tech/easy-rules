d78c34f66616e547fc0f605bc1e8b1f3c0e91b5c
Phelix Ochieng <9phelix1@gmail.com>
2023-10-23 20:05:55 +0300

    Init Project

--- src/main/java/ke/co/phyno/learning/easy/rules/type/RuleType.java
+++ src/main/java/ke/co/phyno/learning/easy/rules/type/RuleType.java
@@ -1,23 +1,7 @@
 package ke.co.phyno.learning.easy.rules.type;
 
-import ke.co.phyno.learning.easy.rules.rules.BaseRule;
-import ke.co.phyno.learning.easy.rules.rules.age.MaximumAgeBaseRule;
-import ke.co.phyno.learning.easy.rules.rules.age.MinimumAgeBaseRule;
-import ke.co.phyno.learning.easy.rules.utils.rules.RuleTypeUtils;
-import lombok.Getter;
-import org.springframework.lang.NonNull;
-
-@Getter
 public enum RuleType {
-    MINIMUM_AGE("", RuleTypeUtils.getBean(MinimumAgeBaseRule.class)),
-    MAXIMUM_AGE("", RuleTypeUtils.getBean(MaximumAgeBaseRule.class)),
+    MINIMUM_AGE,
+    MAXIMUM_AGE,
     ;
-
-    private final String description;
-    private final BaseRule rule;
-
-    RuleType(@NonNull String description, @NonNull BaseRule rule) {
-        this.description = description;
-        this.rule = rule;
-    }
 }
